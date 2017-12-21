a0e3fe2337f4dbb910ad9d18d0cf7664738412e1
kasra-nejad <pfdclover@gmail.com>
2017-12-20 22:48:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  const input = document.querySelector('.input');
+  const fact = document.querySelector('.fact');
+  const factText = document.querySelector('.facttext');
+  input.addEventListener('input', fetchYear);
+
+  function fetchYear() {
+    let year = input.value;
+    let url = `http://numbersapi.com/${year}/year`;
+
+    fetch(url)
+      .then(res => res.text())
+      .then(data => {
+        fact.style.display = "block";
+        factText.innerText = data;
+      })
+      .catch(err => console.log(err));
+  }
+
+}
