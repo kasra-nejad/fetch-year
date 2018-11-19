5425e66df05934788bbfe593455b9fe23efacb49
kasra-nejad <pfdclover@gmail.com>
2018-11-19 11:39:59 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  const input = document.querySelector(".input");
+  const fact = document.querySelector(".fact");
+  const factText = document.querySelector(".factText");
+  input.addEventListener("input", fetchYear);
+
+  function fetchYear() {
+    let year = input.value;
+    let url = `http://numbersapi.com/${year}/year`;
+    if (year != "") {
+      fetch(url)
+        .then(res => res.text())
+        .then(data => {
+          fact.style.display = "block";
+          factText.innerText = data;
+        })
+        .catch(err => console.log(err));
+    }
+  }
+};
